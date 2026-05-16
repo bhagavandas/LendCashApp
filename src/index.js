adfe345e57043b678a611ce17d69cc8aa6f2710e
bhagavandas <bhagavandas.kommu@gmail.com>
2026-05-16 18:06:27 +0530

    Fix error index

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import emailjs from "emailjs-com";
+
 
 // Replace with the Public Key exactly as shown in your EmailJS dashboard
 // emailjs.init("kcsIbWkJni85GuyVL");
