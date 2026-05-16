1c916679484e33a05e46c784f95e46719243c070
bhagavandas <bhagavandas.kommu@gmail.com>
2026-05-16 17:28:00 +0530

    Fix TypeScript version for CRA build

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import React from "react";
 import LenderForm from "./components/LenderForm";
 
 function App() {
   return (
-    <div>
-      <h1>Lending App</h1>
+    <div className="App">
       <LenderForm />
     </div>
   );
 }
-
 export default App;
