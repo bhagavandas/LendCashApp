b96f488e10423d59aaaa2ec4303d061d039890bb
bhagavandas <bhagavandas.kommu@gmail.com>
2026-05-16 16:21:37 +0530

    fixed deployment errors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import React from "react";
+import LenderForm from "./components/LenderForm";
 
 function App() {
   return (
     <div>
       <h1>Lending App</h1>
       <LenderForm />
-      <p> Deployed Successfully</p>
     </div>
   );
 }
